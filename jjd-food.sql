8cd84bfd2b07774f272f873f26665c308f39c2d8
Nicolas <nicolascastiblanco6@gamil.com>
2022-10-17 23:53:27 -0500

    se agrego la base de datos

--- jjd-food.sql
+++ jjd-food.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2022 at 06:11 AM
+-- Generation Time: Oct 17, 2022 at 11:17 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -29,12 +29,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `administracion` (
   `id` int(11) NOT NULL,
-  `Nombre` int(11) NOT NULL,
-  `Apellido` int(11) NOT NULL,
-  `Dependencia` int(11) NOT NULL,
+  `Nombre` varchar(35) NOT NULL,
+  `Apellido` varchar(35) NOT NULL,
+  `Dependencia` varchar(35) NOT NULL,
   `Salario` int(11) NOT NULL,
   `Contraseña` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `administracion`
+--
+
+INSERT INTO `administracion` (`id`, `Nombre`, `Apellido`, `Dependencia`, `Salario`, `Contraseña`) VALUES
+(0, 'Juan ', 'Robles', 'Administracion', 900000, 123);
 
 -- --------------------------------------------------------
 
@@ -44,12 +51,58 @@ CREATE TABLE `administracion` (
 
 CREATE TABLE `contabilidad` (
   `id` int(11) NOT NULL,
-  `Nombre` int(11) NOT NULL,
-  `Apellido` int(11) NOT NULL,
-  `Dependencia` int(11) NOT NULL,
+  `Nombre` varchar(35) NOT NULL,
+  `Apellido` varchar(35) NOT NULL,
+  `Dependencia` varchar(35) NOT NULL,
   `Salario` int(11) NOT NULL,
   `Contraseña` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contabilidad`
+--
+
+INSERT INTO `contabilidad` (`id`, `Nombre`, `Apellido`, `Dependencia`, `Salario`, `Contraseña`) VALUES
+(1, 'Juan ', 'Cespedes', 'Contabilidad', 900000, 123);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dependencias`
+--
+
+CREATE TABLE `dependencias` (
+  `id` int(11) NOT NULL,
+  `Dependecia` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dependencias`
+--
+
+INSERT INTO `dependencias` (`id`, `Dependecia`) VALUES
+(1, 'Administracion'),
+(3, 'Contabilidad'),
+(2, 'Recursos humanos');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mensajes`
+--
+
+CREATE TABLE `mensajes` (
+  `id` int(11) NOT NULL,
+  `Asunto` varchar(45) NOT NULL,
+  `Mensaje` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mensajes`
+--
+
+INSERT INTO `mensajes` (`id`, `Asunto`, `Mensaje`) VALUES
+(1, 'Felicitaciones', 'La comida esta muy rica :D');
 
 -- --------------------------------------------------------
 
@@ -71,22 +124,32 @@ CREATE TABLE `pedido` (
 
 INSERT INTO `pedido` (`id`, `Precio`, `Fecha`, `tipo`, `direccion`) VALUES
 (1, 35000, 'jueves', 'Plazoleta', 'Centro comercial'),
-(1, 50000, 'jueves', 'Plazoleta', 'Centro comercial');
+(1, 50000, 'jueves', 'Plazoleta', 'Centro comercial'),
+(1, 50000, 'sábado', 'Plazoleta', 'Centro comercial'),
+(1, 30000, 'sábado', 'domicilio', 'calle 74 sur'),
+(1, 25000, 'sábado', 'Plazoleta', 'Centro comercial');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `recursos humanos`
+-- Table structure for table `recursos_humanos`
 --
 
-CREATE TABLE `recursos humanos` (
+CREATE TABLE `recursos_humanos` (
   `id` int(11) NOT NULL,
-  `Nombre` int(11) NOT NULL,
-  `Apellido` int(11) NOT NULL,
-  `Dependencia` int(11) NOT NULL,
+  `Nombre` varchar(35) NOT NULL,
+  `Apellido` varchar(35) NOT NULL,
+  `Dependencia` varchar(35) NOT NULL,
   `Salario` int(11) NOT NULL,
   `Contraseña` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `recursos_humanos`
+--
+
+INSERT INTO `recursos_humanos` (`id`, `Nombre`, `Apellido`, `Dependencia`, `Salario`, `Contraseña`) VALUES
+(6, 'Nicolas', 'Perez', 'Recursos humanos', 900000, 123);
 
 -- --------------------------------------------------------
 
@@ -119,13 +182,26 @@ INSERT INTO `usuarios` (`id`, `Nombre`, `nombre_usu`, `apellido_usu`, `edad`, `c
 -- Indexes for table `administracion`
 --
 ALTER TABLE `administracion`
-  ADD KEY `id` (`id`);
+  ADD KEY `Dependencia` (`Dependencia`);
 
 --
 -- Indexes for table `contabilidad`
 --
 ALTER TABLE `contabilidad`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Dependencia` (`Dependencia`);
+
+--
+-- Indexes for table `dependencias`
+--
+ALTER TABLE `dependencias`
+  ADD PRIMARY KEY (`Dependecia`);
+
+--
+-- Indexes for table `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD KEY `id` (`id`);
 
 --
 -- Indexes for table `pedido`
@@ -134,10 +210,11 @@ ALTER TABLE `pedido`
   ADD KEY `id` (`id`);
 
 --
--- Indexes for table `recursos humanos`
+-- Indexes for table `recursos_humanos`
 --
-ALTER TABLE `recursos humanos`
-  ADD KEY `id` (`id`);
+ALTER TABLE `recursos_humanos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Dependencia` (`Dependencia`);
 
 --
 -- Indexes for table `usuarios`
@@ -148,6 +225,18 @@ ALTER TABLE `usuarios`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `contabilidad`
+--
+ALTER TABLE `contabilidad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `recursos_humanos`
+--
+ALTER TABLE `recursos_humanos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `usuarios`
@@ -163,7 +252,19 @@ ALTER TABLE `usuarios`
 -- Constraints for table `administracion`
 --
 ALTER TABLE `administracion`
-  ADD CONSTRAINT `administracion_ibfk_1` FOREIGN KEY (`id`) REFERENCES `contabilidad` (`id`);
+  ADD CONSTRAINT `administracion_ibfk_1` FOREIGN KEY (`Dependencia`) REFERENCES `dependencias` (`Dependecia`);
+
+--
+-- Constraints for table `contabilidad`
+--
+ALTER TABLE `contabilidad`
+  ADD CONSTRAINT `contabilidad_ibfk_1` FOREIGN KEY (`Dependencia`) REFERENCES `dependencias` (`Dependecia`);
+
+--
+-- Constraints for table `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD CONSTRAINT `mensajes_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuarios` (`id`);
 
 --
 -- Constraints for table `pedido`
@@ -172,10 +273,10 @@ ALTER TABLE `pedido`
   ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuarios` (`id`);
 
 --
--- Constraints for table `recursos humanos`
+-- Constraints for table `recursos_humanos`
 --
-ALTER TABLE `recursos humanos`
-  ADD CONSTRAINT `recursos humanos_ibfk_1` FOREIGN KEY (`id`) REFERENCES `contabilidad` (`id`);
+ALTER TABLE `recursos_humanos`
+  ADD CONSTRAINT `recursos_humanos_ibfk_1` FOREIGN KEY (`Dependencia`) REFERENCES `dependencias` (`Dependecia`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
