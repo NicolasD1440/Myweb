354558aaa08b79a67f9a60e33c64cd2cbd12f9d0
JDiegoRG <insa.juanrobles@gmail.com>
2022-11-13 21:07:10 -0500

    xd

---
+++ jjd-food.sql
@@ -0,0 +1,278 @@
+-- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: jjd-food
+-- ------------------------------------------------------
+-- Server version	10.4.24-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `administracion`
+--
+
+DROP TABLE IF EXISTS `administracion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `administracion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(35) NOT NULL,
+  `Apellido` varchar(35) NOT NULL,
+  `Dependencia` varchar(35) NOT NULL,
+  `Salario` int(11) NOT NULL,
+  `Contraseña` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Dependencia` (`Dependencia`),
+  CONSTRAINT `administracion_ibfk_1` FOREIGN KEY (`Dependencia`) REFERENCES `dependencias` (`Dependecia`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `administracion`
+--
+
+LOCK TABLES `administracion` WRITE;
+/*!40000 ALTER TABLE `administracion` DISABLE KEYS */;
+INSERT INTO `administracion` VALUES (1,'Juan','Robles','Administracion',900000,'1234'),(4,'Deivy','Tesla','Administracion',10000000,'Admin2');
+/*!40000 ALTER TABLE `administracion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contabilidad`
+--
+
+DROP TABLE IF EXISTS `contabilidad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contabilidad` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(35) NOT NULL,
+  `Apellido` varchar(35) NOT NULL,
+  `Dependencia` varchar(35) NOT NULL,
+  `Salario` int(11) NOT NULL,
+  `Contraseña` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Dependencia` (`Dependencia`),
+  CONSTRAINT `contabilidad_ibfk_1` FOREIGN KEY (`Dependencia`) REFERENCES `dependencias` (`Dependecia`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contabilidad`
+--
+
+LOCK TABLES `contabilidad` WRITE;
+/*!40000 ALTER TABLE `contabilidad` DISABLE KEYS */;
+INSERT INTO `contabilidad` VALUES (1,'Juan ','Cespedes','Contabilidad',900000,'123');
+/*!40000 ALTER TABLE `contabilidad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dependencias`
+--
+
+DROP TABLE IF EXISTS `dependencias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dependencias` (
+  `id` int(11) NOT NULL,
+  `Dependecia` varchar(45) NOT NULL,
+  PRIMARY KEY (`Dependecia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dependencias`
+--
+
+LOCK TABLES `dependencias` WRITE;
+/*!40000 ALTER TABLE `dependencias` DISABLE KEYS */;
+INSERT INTO `dependencias` VALUES (1,'Administracion'),(3,'Contabilidad'),(2,'Recursos humanos');
+/*!40000 ALTER TABLE `dependencias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mensajes`
+--
+
+DROP TABLE IF EXISTS `mensajes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mensajes` (
+  `id` int(11) NOT NULL,
+  `id_mensaje` int(11) NOT NULL,
+  `Asunto` varchar(45) NOT NULL,
+  `mensaje` varchar(100) NOT NULL,
+  `estado` int(11) NOT NULL,
+  PRIMARY KEY (`id_mensaje`),
+  KEY `id` (`id`),
+  CONSTRAINT `mensajes_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mensajes`
+--
+
+LOCK TABLES `mensajes` WRITE;
+/*!40000 ALTER TABLE `mensajes` DISABLE KEYS */;
+INSERT INTO `mensajes` VALUES (1,2000,'Felicitaciones','La comida ta buena :D',1),(1,2020,'nose','hola',1),(1,2022,'Queja','La comida no esta tan buena :(',0);
+/*!40000 ALTER TABLE `mensajes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedido`
+--
+
+DROP TABLE IF EXISTS `pedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pedido` (
+  `id` int(11) NOT NULL,
+  `Precio` int(11) NOT NULL,
+  `Fecha` varchar(25) NOT NULL,
+  `tipo` varchar(25) NOT NULL,
+  `direccion` varchar(35) NOT NULL,
+  `det_fecha` date NOT NULL,
+  KEY `id` (`id`),
+  CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedido`
+--
+
+LOCK TABLES `pedido` WRITE;
+/*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `platos`
+--
+
+DROP TABLE IF EXISTS `platos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `platos` (
+  `Titulo` varchar(200) NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `precio` bigint(25) NOT NULL,
+  `imagen` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `platos`
+--
+
+LOCK TABLES `platos` WRITE;
+/*!40000 ALTER TABLE `platos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `platos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recursos_humanos`
+--
+
+DROP TABLE IF EXISTS `recursos_humanos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `recursos_humanos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(35) NOT NULL,
+  `Apellido` varchar(35) NOT NULL,
+  `Dependencia` varchar(35) NOT NULL,
+  `Salario` int(11) NOT NULL,
+  `Contraseña` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Dependencia` (`Dependencia`),
+  CONSTRAINT `recursos_humanos_ibfk_1` FOREIGN KEY (`Dependencia`) REFERENCES `dependencias` (`Dependecia`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recursos_humanos`
+--
+
+LOCK TABLES `recursos_humanos` WRITE;
+/*!40000 ALTER TABLE `recursos_humanos` DISABLE KEYS */;
+INSERT INTO `recursos_humanos` VALUES (6,'Nicolas','Perez','Recursos humanos',900000,'123'),(7,'Juan','Robles','Recursos humanos',50,'Cont2');
+/*!40000 ALTER TABLE `recursos_humanos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `respuesta`
+--
+
+DROP TABLE IF EXISTS `respuesta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `respuesta` (
+  `id-mensaje` int(11) NOT NULL,
+  `Asunto` varchar(25) NOT NULL,
+  `Mensaje` varchar(100) NOT NULL,
+  KEY `id-mensaje` (`id-mensaje`),
+  CONSTRAINT `respuesta_ibfk_2` FOREIGN KEY (`id-mensaje`) REFERENCES `mensajes` (`id_mensaje`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `respuesta`
+--
+
+LOCK TABLES `respuesta` WRITE;
+/*!40000 ALTER TABLE `respuesta` DISABLE KEYS */;
+INSERT INTO `respuesta` VALUES (2020,'Respuesta','hola'),(2000,'Respuesta','hola');
+/*!40000 ALTER TABLE `respuesta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(35) NOT NULL,
+  `nombre_usu` varchar(25) NOT NULL,
+  `apellido_usu` varchar(25) NOT NULL,
+  `edad` int(11) NOT NULL,
+  `correo` varchar(25) NOT NULL,
+  `Telefono` bigint(20) NOT NULL,
+  `contraseña` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Nicolas Castiblanco','Nicolas ','Castiblanco',19,'nicolas@gmail.com',84683813,'user1');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-13 21:01:47
